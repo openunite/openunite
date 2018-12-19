2e8c37373c77e5537fd26d1b195cd735ca1205bf
Luis Farzati <lufohs@gmail.com>
2018-12-20 00:25:19 +0100

    Refactor login for dependency injection; also add integration tests

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -1,15 +1,31 @@
 import Koa from "koa";
-import * as AuthService from "../services/AuthService";
-export let login = async (ctx: Koa.Context) => {
-  let { token } = await AuthService.login({
-    username: ctx.request.body.username,
-    password: ctx.request.body.password
-  });
+import { AuthService } from "../services/AuthService";
 
-  if (token) {
-    ctx.body = { token };
-  } else {
-    ctx.status = 401;
-    ctx.body = { error: "Invalid User or Password" };
+class AuthController {
+  private authService: AuthService;
+
+  constructor(authService: any) {
+    this.authService = authService;
   }
-};
+
+  async login(ctx: Koa.Context) {
+    const { username = "", password = "" } = ctx.request.body;
+
+    if (username.trim() === "" || password.trim() === "") {
+      ctx.status = 400;
+      ctx.body = { error: "Missing username and/or password" };
+      return;
+    }
+
+    const { token } = await this.authService.login({ username, password });
+
+    if (token) {
+      ctx.body = { token };
+    } else {
+      ctx.status = 401;
+      ctx.body = { error: "Invalid User or Password" };
+    }
+  }
+}
+
+export { AuthController };
--- api/test/API.test.ts
+++ api/test/API.test.ts
@@ -1,15 +1,28 @@
+import "should";
 import supertest from "supertest";
 import { createServer } from "../src/Server";
 import { createRouter } from "../src/Router";
+import { AuthController } from "../src/controllers/AuthController";
+import { AuthService } from "../src/services/AuthService";
+
+const testHttpPort = 7979;
 
 describe("API", () => {
+  const testCredentials = {
+    username: "test-user",
+    password: "test-password"
+  };
+  const testJwtSecret = "test-secret";
+
   let request: supertest.SuperTest<supertest.Test>;
   let server: import("http").Server;
 
   beforeEach(async () => {
-    const router = createRouter();
+    const authService = new AuthService(testCredentials, testJwtSecret);
+    const authController = new AuthController(authService);
+    const router = createRouter(authController);
     const app = createServer(router);
-    server = app.listen(7979);
+    server = app.listen(testHttpPort);
     request = supertest(server);
   });
 
@@ -18,8 +31,58 @@ describe("API", () => {
   });
 
   describe("/health", () => {
-    it("should return 200 response", () => {
-      return request.get("/health").expect(200);
+    it("should return 200 response", async () => {
+      // act
+      const response = await request.get("/health");
+
+      // assert
+      response.status.should.equal(200);
+    });
+  });
+
+  describe("/login", () => {
+    describe("When credentials are missing", () => {
+      it("should return 400 response", async () => {
+        // act
+        const response = await request.post("/login");
+
+        // assert
+        response.status.should.equal(400);
+      });
+    });
+
+    describe("When credentials are invalid", () => {
+      it("should return 401 response", async () => {
+        // arrange
+        const username = "invalid-username";
+        const password = "invalid-password";
+
+        // act
+        const response = await request
+          .post("/login")
+          .send({ username, password });
+
+        // assert
+        response.status.should.equal(401);
+        response.body.should.have.property("error").which.is.a.String();
+      });
+    });
+
+    describe("When credentials are valid", () => {
+      it("should return 200 response", async () => {
+        // arrange
+        const username = testCredentials.username;
+        const password = testCredentials.password;
+
+        // act
+        const response = await request
+          .post("/login")
+          .send({ username, password });
+
+        // assert
+        response.status.should.equal(200);
+        response.body.should.have.property("token").which.is.a.String();
+      });
     });
   });
 });
