c46088b04380a63d4576e03533c5170ef51e9f00
Luis Farzati <lufohs@gmail.com>
2018-12-19 15:58:26 +0100

    Add health endpoint

--- api/src/Router.ts
+++ api/src/Router.ts
@@ -3,6 +3,11 @@ import KoaRouter from "koa-router";
 function createRouter() {
   const router = new KoaRouter();
 
+  // Health endpoint
+  router.get("/health", (ctx) => {
+    ctx.status = 200;
+  });
+
   return router;
 }
 
