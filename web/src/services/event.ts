82d87ea84e6f4ae7fa3ab8585fcd953887d86d7d
Luis Farzati <lufohs@gmail.com>
2018-12-21 11:09:46 +0100

    Make EventList component fetch events from the API

---
+++ web/src/services/event.ts
@@ -0,0 +1,23 @@
+import ApiService from "./api";
+import moment from 'moment';
+
+const EventService = {
+  async getEvents() {
+    const api = new ApiService();
+
+    let events;
+    try {
+      events = await api.get("events");
+    } catch (err) {
+      console.log(err);
+    }
+
+    return events.map((event: any) => ({
+      ...event,
+      date: moment(event.date)
+    }))
+
+  }
+};
+
+export default EventService;
