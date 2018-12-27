9a49afe7ac22ed0c7008fdd1979b0b08d0c29498
Alan Reid <mail@alanreid.de>
2018-12-27 16:43:15 +0100

    Refactor camel-cased class names (#53)
    * Refactor camelcased class names
    
    * Fix event list class case
    
    * Fix event list class case

--- web/src/components/event/EventList.tsx
+++ web/src/components/event/EventList.tsx
@@ -14,7 +14,7 @@ class EventList extends Component<EventListProps> {
       <div>
         {this.props.events.map((event: GroupEvent) => {
           return (
-            <Card key={event.id} className="eventItem">
+            <Card key={event.id} className="event-item">
               <span className="date">{event.date.format("dddd, MMMM M ")}</span>
               <h5>{event.group.name}</h5>
               <h2>{event.title}</h2>
